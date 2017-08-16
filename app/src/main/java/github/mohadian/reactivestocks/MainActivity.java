2065326060ad9d86b1a008fb6bb788f9b044a6c0
Mostafa Hadian <Mostafa.HadianDehkordi@sky.uk>
2017-08-16 10:30:55 +0100

    Add recyclerview

--- app/src/main/java/github/mohadian/reactivestocks/MainActivity.java
+++ app/src/main/java/github/mohadian/reactivestocks/MainActivity.java
@@ -2,10 +2,16 @@ package github.mohadian.reactivestocks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import github.mohadian.reactivestocks.adapter.StockDataManager;
+import github.mohadian.reactivestocks.data.StockUpdate;
 import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
 
@@ -13,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.hello_world_salute)
     TextView textView;
+    @BindView(R.id.stock_updates_recycler_view)
+    RecyclerView recyclerView;
+
+    private LinearLayoutManager linearLayoutManager;
+    private StockDataManager stockDataManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(s);
             }
         });
+
+        prepareRecyclerView();
+
+        Observable.just(new StockUpdate("GOOGLE", 12.43, new Date()), new StockUpdate("APPL", 645.1, new Date()), new StockUpdate("TWTR", 1.43, new Date())
+
+        ).subscribe(new Consumer<StockUpdate>() {
+            @Override
+            public void accept(StockUpdate stockSymbol) {
+                stockDataManager.add(stockSymbol);
+            }
+        });
+    }
+
+    private void prepareRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        stockDataManager = new StockDataManager();
+        recyclerView.setAdapter(stockDataManager);
     }
 }
